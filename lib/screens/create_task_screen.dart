fd82ad4dba42d7f91e3d6199ce975b4823689ddf
sellyazura <sellyazura2@gmail.com>
2025-07-24 15:43:53 +0700

    edit by selly

--- lib/screens/create_task_screen.dart
+++ lib/screens/create_task_screen.dart
@@ -37,6 +37,27 @@ class CreateTaskScreen extends StatelessWidget {
                     bottomRight: Radius.circular(30),
                   ),
                 ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Icon(Icons.arrow_back),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: const[
+                          Text(
+                            'Add New Task',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      )
+                      )
+                  ],
+                ),
               ),
             ],
           ),
